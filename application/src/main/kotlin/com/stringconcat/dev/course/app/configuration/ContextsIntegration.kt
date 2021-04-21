c8bd140ed6cd5d01cf3a7293873a5a07680719b0
Dmitrii Abramov <karvozavr@gmail.com>
2021-04-21 14:03:46 +0300

    Add rules (domain event listeners) to context integration config

--- application/src/main/kotlin/com/stringconcat/dev/course/app/configuration/ContextsIntegration.kt
+++ application/src/main/kotlin/com/stringconcat/dev/course/app/configuration/ContextsIntegration.kt
@@ -1,11 +1,15 @@
 package com.stringconcat.dev.course.app.configuration
 
+import com.stringconcat.ddd.delivery.usecase.order.CreateOrder
 import com.stringconcat.ddd.kitchen.usecase.order.CreateOrderHandler
 import com.stringconcat.ddd.order.usecase.menu.MealExtractor
+import com.stringconcat.ddd.order.usecase.order.CompleteOrder
 import com.stringconcat.ddd.order.usecase.order.CustomerOrderExtractor
 import com.stringconcat.ddd.order.usecase.payment.ExportPaymentData
 import com.stringconcat.dev.course.app.event.EventPublisherImpl
+import com.stringconcat.dev.course.app.listeners.CompleteOrderWhenDeliveredRule
 import com.stringconcat.dev.course.app.listeners.ExportSuccessfulPaymentToCrmRule
+import com.stringconcat.dev.course.app.listeners.SendOrderToDeliveryWhenCookedRule
 import com.stringconcat.dev.course.app.listeners.SendOrderToKitchenAfterConfirmationRule
 import com.stringconcat.integration.crm.SimpleCRMConnector
 import org.springframework.context.annotation.Bean
@@ -39,6 +43,34 @@ class ContextsIntegration {
     ): ExportSuccessfulPaymentToCrmRule {
         val listener = ExportSuccessfulPaymentToCrmRule(
             exportPaymentData = exportPaymentData
+        )
+
+        domainEventPublisher.registerListener(listener)
+        return listener
+    }
+
+    @Bean
+    fun sendOrderToDeliveryWhenCookedRule(
+        customerOrderExtractor: CustomerOrderExtractor,
+        createOrderUseCase: CreateOrder,
+        domainEventPublisher: EventPublisherImpl
+    ): SendOrderToDeliveryWhenCookedRule {
+        val listener = SendOrderToDeliveryWhenCookedRule(
+            customerOrderExtractor = customerOrderExtractor,
+            createOrderUseCase = createOrderUseCase
+        )
+
+        domainEventPublisher.registerListener(listener)
+        return listener
+    }
+
+    @Bean
+    fun sendOrderToDeliveryWhenCookedRule(
+        completeOrderUseCase: CompleteOrder,
+        domainEventPublisher: EventPublisherImpl
+    ): CompleteOrderWhenDeliveredRule {
+        val listener = CompleteOrderWhenDeliveredRule(
+            completeOrderUseCase = completeOrderUseCase
         )
 
         domainEventPublisher.registerListener(listener)
